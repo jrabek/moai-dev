46eae2195a6490c3b496d9c8815757beebb78072
Jesse Rabek <jrabek@gmail.com>
2012-09-07 11:03:51 -0300

    MOAICollsionSet sample

---
+++ samples/simple-collision/main.lua
@@ -0,0 +1,59 @@
+----------------------------------------------------------------
+-- Copyright (c) 2010-2011 Zipline Games, Inc. 
+-- All Rights Reserved. 
+-- http://getmoai.com
+----------------------------------------------------------------
+
+MOAISim.openWindow ( "test", 320, 480 )
+
+MOAIDebugLines.setStyle ( MOAIDebugLines.PROP_MODEL_BOUNDS, 2, 1, 1, 1 )
+MOAIDebugLines.setStyle ( MOAIDebugLines.PROP_WORLD_BOUNDS, 2, 0.75, 0.75, 0.75 )
+
+viewport = MOAIViewport.new ()
+viewport:setSize ( 320, 480 )
+viewport:setScale ( 320, -480 )
+
+layer = MOAILayer2D.new ()
+layer:setViewport ( viewport )
+MOAISim.pushRenderPass ( layer )
+
+gfxQuad = MOAIGfxQuad2D.new ()
+gfxQuad:setTexture ( "moai.png" )
+gfxQuad:setRect ( -32, -32, 32, 32 )
+gfxQuad:setUVRect ( 0, 0, 1, 1 )
+
+prop1 = MOAIProp2D.new ()
+prop1:setDeck ( gfxQuad )
+prop1:setLoc ( -128, -128 )
+ease1 = prop1:moveLoc ( 256, 256, 5 , MOAIEaseType.LINEAR)
+layer:insertProp ( prop1 )
+
+prop2 = MOAIProp2D.new ()
+prop2:setDeck ( gfxQuad )
+prop2:setRot ( 45 )
+prop2:setLoc ( 0, 0 )
+layer:insertProp ( prop2 )
+
+r = 0
+g = 0
+b = 0
+
+function collisionHandler(prop1, prop2)
+	prop1:setColor ( 0.5 + r, b, 0, 0 )
+	prop2:setColor ( r, 0.5+g, 0.25+b, 0 )
+	
+	ease1:stop()
+	prop1:setLoc ( -128, -128 )
+	ease1 = prop1:moveLoc ( 256, 256, 5 , MOAIEaseType.LINEAR)
+	
+	r = r + 0.2
+	g = g + 0.2
+	b = b + 0.2
+	
+end
+
+collisionSet = MOAICollisionSet.new ()
+collisionSet:setCollisionHandler(collisionHandler)
+collisionSet:insertProp(prop1)
+collisionSet:insertProp(prop2)
+collisionSet:start()
